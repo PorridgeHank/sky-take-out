4430c742fd956e98685041449d0f738995166c4a
porhank <9809334+porhank@user.noreply.gitee.com>
2025-03-06 11:17:37 +0800

    文件上传、新增菜品功能

---
+++ sky-server/src/main/java/com/sky/config/MinioConfiguration.java
@@ -0,0 +1,22 @@
+package com.sky.config;
+
+import com.sky.properties.MinioProperties;
+import com.sky.utils.MinioUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class MinioConfiguration {
+    @Bean
+    @ConditionalOnMissingBean
+    public MinioUtil aliOssUtil(MinioProperties minioProperties){
+        log.info("开始创建阿里云服务器Minio上传工具类对象：{}", minioProperties);
+        return new MinioUtil(minioProperties.getEndpoint(),
+                minioProperties.getAccessKey(),
+                minioProperties.getSecretKey(),
+                minioProperties.getBucketName());
+    }
+}
